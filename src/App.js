ecedd145c9369bd9a8c9eef7cbc86295b02f3635
Marcelo Jose Batista Fajardo <chelobat16411@gmail.com>
2022-06-28 18:45:47 -0300

    Gretting deleted

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import { Box } from "@mui/material";
-import { Navbar } from "./components";
-import { ExerciseDetail, Home } from "./pages";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { Navbar } from './components';
+import { ExerciseDetail, Home } from './pages';
 
 const App = () => {
   return (
-    <Box width="401px" sx={{ width: { xl: "1488px" } }} m="auto">
+    <Box width="401px" sx={{ width: { xl: '1488px' } }} m="auto">
       <Navbar />
-      <h1>Gretting</h1>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/exercise/:id" element={<ExerciseDetail />} />
